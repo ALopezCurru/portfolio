97bd4182c5db097aa626076779ec335bf31ff8f2
AlexDebianTemporal <alexlopez.curru@gmail.com>
2026-04-19 03:22:10 -0500

    Agregando formulario de contacto

--- utils/domElements.js
+++ utils/domElements.js
@@ -3,5 +3,8 @@
 export const DOM = {
   contenedorStack: document.querySelector("#contenedorStack"),
   contenedorProjects: document.querySelector('#contenedorProjectos'),
-  contenedorExperience: document.querySelector('#containerLaboralExperience')
+  contenedorExperience: document.querySelector('#containerLaboralExperience'),
+  btnContactMe: document.querySelectorAll('.contactMeForm'),
+  mainContainer: document.querySelector('#mainSection'),
+  modalContactForm: document.querySelector('#mainSection')
 };
